c36e0a144d2998bc2ece3c3fdd49c9bb7f8d0a0d
andy <andy.ukpe@outlook.com>
2020-06-25 00:05:09 +0200

    appendere oggetti ad html

--- js/script.js
+++ js/script.js
@@ -7,57 +7,108 @@
 
 //creo un oggetto studente con nome, cognome, età
 
-var studente = {
-  nome : 'Carlo',
-  cognome : 'Manfredi',
-  eta : '21',
-};
+// var studente = {
+//   nome : 'Carlo',
+//   cognome : 'Manfredi',
+//   eta : '21',
+// };
+//
+// // stampare l'oggetto attraverso un ciclo for-in
+//
+// for (var chiave in studente){
+//   console.log(studente[chiave]);
+// }
+//
+// // creare un array di oggetti di studenti
+//
+// var studenti = [
+//   {
+//     'nome': 'Carlo',
+//     'cognome': 'Manfredi',
+//     'eta' : '21',
+//   },
+//   {
+//     'nome': 'Serse',
+//     'cognome': 'Mulini',
+//     'eta' : '19',
+//   },
+//   {
+//     'nome': 'Lara',
+//     'cognome': 'Camerati',
+//     'eta' : '20',
+//   },
+//   {
+//     'nome': 'Erica',
+//     'cognome': 'Wilt',
+//     'eta' : '23',
+//   },
+// ]
+//
+// // ciclare tutti gli studenti e stampare per oguno nome e cognome
+//
+// for (var i = 0; i < studenti.length; i++){
+//   console.log(studenti[i]);
+//   console.log('Nome studente:' + studenti[i].nome);
+//   console.log('Cognome studente:' + studenti[i].cognome);
+// }
+//
+// // far inserire all'utente nome, cognome e eta nell'oggetto per poi metterlo nel push
+// var nomeUtente = prompt('inserisci il tuo nome');
+// var cognomeUtente = prompt('inserisci il tuo cognome');
+// var etaUtente = prompt('inserisci il tuo eta');
+// var nuovoStudente = {
+//   nome: nomeUtente,
+//   cognome: cognomeUtente,
+//   eta: etaUtente,
+// }
+// studenti.push(nuovoStudente);
+// console.log(studenti)
+//
+// for (var i = 0; i < studenti.length; i++){
+//   var studente = studenti[i];
+//   console.log(studenti[i]);
+//   console.log('Nome studente:' + studente.nome);
+//   console.log('Cognome studente:' + studente.cognome);
+//   console.log('età studente:'+ studente.eta)
+// }
+// stampare risultati attraverso handelbars
+$(document).ready(function(){
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
 
-// stampare l'oggetto attraverso un ciclo for-in
+  var studentiData = [
+    {
+      'nome': 'Carlo',
+      'cognome': 'Manfredi',
+      'eta' : '21',
+    },
+    {
+      'nome': 'Serse',
+      'cognome': 'Mulini',
+      'eta' : '19',
+    },
+    {
+      'nome': 'Lara',
+      'cognome': 'Camerati',
+      'eta' : '20',
+    },
+    {
+      'nome': 'Erica',
+      'cognome': 'Wilt',
+      'eta' : '23',
+    },
+  ]
 
-for (var chiave in studente){
-  console.log(studente[chiave]);
+  for (var i = 0; i < studentiData.length; i++) {
+    var singoloStudente = studentiData[i];
+    var html = template(singoloStudente);
+
+    $('.container').append(html);
+  }
+  }
+  studenti.push(nuovoStudente);
+  console.log(studenti)
 }
 
-// creare un array di oggetti di studenti
 
-var studenti = [
-  {
-    'nome': 'Carlo',
-    'cognome': 'Manfredi',
-    'eta' : '21',
-  },
-  {
-    'nome': 'Serse',
-    'cognome': 'Mulini',
-    'eta' : '19',
-  },
-  {
-    'nome': 'Lara',
-    'cognome': 'Camerati',
-    'eta' : '20',
-  },
-  {
-    'nome': 'Erica',
-    'cognome': 'Wilt',
-    'eta' : '23',
-  },
-]
-
-// ciclare tutti gli studenti e stampare per oguno nome e cognome
-
-for (var i = 0; i < studenti.length; i++){
-  console.log(studenti[i]);
-  console.log('Nome studente:' + studenti[i].nome);
-  console.log('Cognome studente:' + studenti[i].cognome);
-}
-
-var nome = prompt('inserisci il tuo nome');
-var nomeStudente =studenti.nome;
-nomeStudente.push(nome);
-
-for (var i = 0; i < studenti.length; i++){
-  console.log(studenti[i]);
-  console.log('Nome studente:' + studenti[i].nome);
-  console.log('Cognome studente:' + studenti[i].cognome);
-}
+)
